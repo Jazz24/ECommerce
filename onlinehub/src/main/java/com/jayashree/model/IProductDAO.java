6dcdbc061bad5021054b9bffebb8c42e231cbd93
Jazz24 <30719851+Jazz24@users.noreply.github.com>
2018-04-03 17:17:28 +0530

    Add files via upload
    My First Web App

---
+++ onlinehub/src/main/java/com/jayashree/model/IProductDAO.java
@@ -0,0 +1,14 @@
+package com.jayashree.model;
+
+import java.util.List;
+
+public interface IProductDAO {
+
+	boolean insertProduct(Product p);
+	List<Product> getProducts();
+	Product getProduct(int p);
+	boolean updateProduct(Product p);
+	boolean deleteProduct(Product p);
+
+}
+
